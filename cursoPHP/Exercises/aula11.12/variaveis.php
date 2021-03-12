610cc2f8361fea2e046f0c0fb21a144e3d0d36ad
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2021-03-12 08:07:40 -0300

    first commit

---
+++ cursoPHP/Exercises/aula11.12/variaveis.php
@@ -0,0 +1,23 @@
+<?php
+    //estudo de variaveis em php
+
+    //variaveis strings
+
+    $nome = "Guilherme Henrique";
+    echo $nome;
+    echo "<br>";
+    //variavel int ou INTEGER
+
+    $n1 = 50;
+    echo $n1;
+    echo "<br>";
+    //variavel float
+
+    $n2 = 50.99;
+    echo $n2;
+    echo "<br>";
+    
+    echo "Guilherme Henrique $n1 $n2";
+    
+
+?>
---
+++ cursoPHP/Exercises/aula15/arrays.php
@@ -0,0 +1,66 @@
+<?php
+
+    //Estudo sobre arrays
+
+
+echo "<h1>Estudo sobre arrays</hr>";
+
+$frutas = array("maçã","banana","mamão", "melão", "abacate"); 
+
+
+$produtos = array(
+    array(
+        "nome"=>"Macarrão 400g", 
+        "preco" => 3.99,
+        "qtd" => 2
+    ),
+
+    array(
+        "nome"=>"Biscoito", 
+        "preco" => 1.99,
+        "qtd" => 3
+    ),
+
+    array(
+        "nome"=>"Sabão em Pó", 
+        "preco" => 6.99,
+        "qtd" => 3
+    )
+
+);
+
+//faz um push no array
+array_push($frutas, "Melancia");
+array_push($frutas, "Pêra");
+array_push($frutas, "Uva");
+
+echo "<pre>";
+//exibe o array todo e suas posições
+print_r($produtos);
+echo '<br/>';
+//exibe o array todo, suas posições e seus tipos
+var_dump($produtos);
+echo "</pre>";
+
+
+//Percorre todo o array 
+foreach($frutas as $frutas){
+    //echo "<br>";
+    //echo $frutas;
+}
+ 
+echo "<hr>";
+echo "<h2>Arrays associativos<h2/>";
+
+$cliente = array(
+            "name" => "Guilherme", 
+            "fone" => "(55)555-5555", 
+            "email" => "contato@curso.com"
+        );
+
+/* echo $cliente['name'];
+echo "<br>";
+echo $cliente['fone']; */
+
+
+
